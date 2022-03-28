8630352647a40a7612500bd4f31bc7fd38cb76d9
Nilay <54245048+dnilay@users.noreply.github.com>
2022-03-28 14:23:56 +0530

    Add files via upload

---
+++ myfirstproject/src/myfirstproject/Maijn.java
@@ -0,0 +1,18 @@
+package myfirstproject;
+
+public class Maijn {
+
+	public static void main(String[] args) {
+	
+			Shirt myShirt;
+			myShirt=new Shirt();
+			myShirt.createShirt();
+			myShirt.displayShirt();
+			Shirt yourShirt;
+			yourShirt=new Shirt();
+			yourShirt.createShirt();
+			yourShirt.displayShirt();
+	
+	}
+
+}
---
+++ myfirstproject/src/myfirstproject/Shirt.java
@@ -0,0 +1,24 @@
+package myfirstproject;
+//name of the class
+//class declaration
+public class Shirt {
+	//attributes
+	int shirtId;
+	String shirtType;
+	int price;
+	
+	//method or operation
+	void createShirt()
+	{
+		System.out.println("short created");
+	}
+	
+	//method or operation
+	void displayShirt()
+
+	{
+		System.out.println("short displayed");
+	}
+	
+
+}
