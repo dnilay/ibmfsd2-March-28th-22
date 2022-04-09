e158b0f08dfd657067ec0567de40925bd28a6ade
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-09 16:39:39 +0530

    Add files via upload

--- ioc-demo2/src/main/java/org/example/demo/App.java
+++ ioc-demo2/src/main/java/org/example/demo/App.java
@@ -1,5 +1,12 @@
 package org.example.demo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
 import org.example.demo.config.SpringConfig;
 import org.example.demo.model.Department;
 import org.example.demo.model.Employee;
@@ -10,30 +17,40 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Hello world!
  *
  */
-public class App 
-{
-    private static ApplicationContext context;
+public class App {
+	private static ApplicationContext context;
 
-	public static void main( String[] args )
-    {
-       try {
-    	   
+	public static void main(String[] args) {
+		try {
+
 			context = new AnnotationConfigApplicationContext(SpringConfig.class);
-    	   Employee myEmployee=context.getBean("employee",Employee.class);
-    	   Department department=context.getBean("department",Department.class);
 			/*
+			 * Employee myEmployee=context.getBean("employee",Employee.class); Department
+			 * department=context.getBean("department",Department.class);
+			 * 
 			 * department.setDepartmentName("Accountind");
 			 * department.setLocation("Kolkata");
+			 * 
+			 * myEmployee.setEmployeeId(100); myEmployee.setEmployeeName("John Doe");
+			 * myEmployee.setSalary(10000); myEmployee.setDepartment(department);
+			 * System.out.println(myEmployee);
 			 */
-    	   myEmployee.setEmployeeId(100);
-    	   myEmployee.setEmployeeName("John Doe");
-    	   myEmployee.setSalary(10000);
-    	   myEmployee.setDepartment(department);
-    	   System.out.println(myEmployee);
-		
-	} catch (Exception e) {
-		// TODO: handle exception
-		e.printStackTrace();
+			
+			DataSource dataSource=context.getBean("dataSource",DataSource.class);
+			Connection connection=dataSource.getConnection();
+			System.out.println(connection);
+		//	PreparedStatement preparedStatement=connection.prepareStatement("insert into employee(name) values('Marry')");
+			//preparedStatement.executeUpdate();
+			//System.out.println("inseerted");
+			Statement statement=connection.createStatement();
+			ResultSet rs= statement.executeQuery("select * from employee");
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1)+" "+rs.getString(2));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
-    }
 }
--- ioc-demo2/src/main/java/org/example/demo/config/SpringConfig.java
+++ ioc-demo2/src/main/java/org/example/demo/config/SpringConfig.java
@@ -1,10 +1,25 @@
 package org.example.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.demo")
 public class SpringConfig {
+	
+	
+	@Bean
+	public DataSource dataSource()
+	{
+		DriverManagerDataSource driverManagerDataSource=new DriverManagerDataSource();
+		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/hr");
+		driverManagerDataSource.setUsername("root");
+		driverManagerDataSource.setPassword("root");
+		return driverManagerDataSource;
+	}
 
 }
