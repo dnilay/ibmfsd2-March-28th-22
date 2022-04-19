34a922b5f8d0dd16ad5af21156673be2632f4bce
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-19 12:05:54 +0530

    Add files via upload

---
+++ crm-app/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/list")
+    public String getAllCustomers(Model model)
+    {
+        List<Customer> list=customerRepository.findAll();
+        model.addAttribute("customers",list);
+        return "customer-list";
+    }
+}
