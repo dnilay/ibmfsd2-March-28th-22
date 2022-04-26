a37009c8652b123078e7aee30f5dd940f7d59092
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-26 10:31:44 +0530

    Add files via upload

--- contact-service/src/main/java/com/example/demo/repo/ContactRepo.java
+++ contact-service/src/main/java/com/example/demo/repo/ContactRepo.java
@@ -1,5 +1,7 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.example.demo.model.Contact;
 
 @Repository
 public interface ContactRepo extends JpaRepository<Contact, Integer> {
+
+	public List<Contact> findByAddress(String address);
+	public List<Contact> findByContactName(String contactName);
 
 }
