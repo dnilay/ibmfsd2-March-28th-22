8eccd9577f9ef35c260a210c0f7f9f9356bb6246
Nilay <54245048+dnilay@users.noreply.github.com>
2022-05-17 11:04:43 +0530

    Add files via upload

---
+++ spring-boot-server-app/src/main/java/com/example/demo/SpringBootServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServerAppApplication.class, args);
+    }
+
+}
---
+++ spring-boot-server-app/src/main/java/com/example/demo/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
